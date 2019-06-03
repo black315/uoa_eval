4b723b6a32831762489b83c46d1f6048163fd6b1
black <ryo.0301.1999@gmail.com>
2019-06-03 15:34:32 +0900

    add subject model

---
+++ db/migrate/20190603063301_create_subjects.rb
@@ -0,0 +1,11 @@
+class CreateSubjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subjects do |t|
+      t.string :name
+      t.string :instructor
+      t.int :rate
+
+      t.timestamps
+    end
+  end
+end
