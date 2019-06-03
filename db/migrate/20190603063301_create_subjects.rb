c769395e96ad5286fb8b73537824a709fb8810b6
black <ryo.0301.1999@gmail.com>
2019-06-03 22:13:32 +0900

    fix subject list page

--- db/migrate/20190603063301_create_subjects.rb
+++ db/migrate/20190603063301_create_subjects.rb
@@ -3,7 +3,7 @@ class CreateSubjects < ActiveRecord::Migration[5.2]
     create_table :subjects do |t|
       t.string :name
       t.string :instructor
-      t.int :rate
+      t.integer :rate
 
       t.timestamps
     end
