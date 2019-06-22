e01bbd02814f93aae36e1f22bda83f61e931c163
black <ryo.0301.1999@gmail.com>
2019-06-23 06:18:49 +0900

    add pagetop button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,4 +32,21 @@ $(function() {
 
         $newForm.toggle('slow');
     });
+
+    let topBtn=$('#pageTop');
+    topBtn.hide();
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>80){
+            topBtn.fadeIn();
+        }else{
+            topBtn.fadeOut();
+        }
+    });
+
+    topBtn.click(function(){
+        $('body,html').animate({
+            scrollTop: 0},500);
+        return false;
+    });
 });
