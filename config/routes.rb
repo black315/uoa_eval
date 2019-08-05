3efd9d56fa51dbe741eb8c47fd5e0389adeef954
black <ryo.0301.1999@gmail.com>
2019-08-05 16:39:19 +0900

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'subjects#index'
   get 'subjects/index'
   get 'subjects/show/:id' => 'subjects#show', as: :subjects_show
   post 'subjects/create' => 'subjects#create'
